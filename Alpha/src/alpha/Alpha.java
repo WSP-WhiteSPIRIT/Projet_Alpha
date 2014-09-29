258d54827be0fd6aaf5dc6cd720952a3683f7755
INTI0224 <INTI0224@192.168.30.142>
2014-09-29 14:12:09 +0200

    helloworld sout

--- Alpha/src/alpha/Alpha.java
+++ Alpha/src/alpha/Alpha.java
@@ -15,7 +15,7 @@ public class Alpha {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("helloworld");
     }
     
 }
