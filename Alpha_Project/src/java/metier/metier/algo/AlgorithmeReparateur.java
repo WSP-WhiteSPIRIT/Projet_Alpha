b4112b0177586fc01949e819f6fce253030717db
INTI0220 <INTI0220@192.168.30.139>
2014-10-01 14:44:00 +0200

    -Ajout de l'agorithme de sélection du réparateur

---
+++ Alpha_Project/src/java/metier/metier/algo/AlgorithmeReparateur.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package metier.metier.algo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author INTI0220
+ */
+public class AlgorithmeReparateur {
+    
+    private List<Reparateur> reparateursDisponibles= new LinkedList<>();
+    private List<Reparateur> reparateursPotentiels;
+    private Reparateur reparateurSelectionne;
+    
+    
+    public Reparateur selectReparateurACharger(Marque marque, Departement departement){
+        loadReparateursPotentiels();
+        
+        selectReparateursDisponibles(Marque marque, Departement departement);
+        
+        selectBestReparateur();
+        return reparateurSelectionne;
+    }
+    
+    public void loadReparateursPotentiels(){
+        ManagerReparateur managerReparateur = new ManagerReparateur();
+        reparateursPotentiels = managerReparateur.getAll();
+    }
+    
+    public void selectReparateursDisponibles(Marque marque, Departement departement){
+        List<Reparateur> reparateursMarque = new ArrayList<Reparateur>() ;
+        for (Reparateur reparateur : reparateursPotentiels) {
+            for (Marque marqueRep : reparateur.getMarques()) {
+                if(marqueRep.equals(marque)){
+                    reparateursMarque.add(reparateur);
+                }
+            }
+        }
+        
+        for (Reparateur reparateur : reparateursMarque) {
+            for (Departement departementRep : reparateur.getDepartements()) {
+                if(departementRep.equals(departement)){
+                    reparateursDisponibles.add(reparateur);
+                }
+            }
+        }
+    }
+    
+    public void selectBestReparateur(){
+        int chargeLibre=0;
+        
+        for (Reparateur reparateur : reparateursDisponibles) {
+            int chargeLibreRep=reparateur.getChargeMax()-reparateur.getCharge();
+            if(chargeLibreRep>chargeLibre){
+                chargeLibre=chargeLibreRep;
+                reparateurSelectionne=reparateur;
+            }
+        }
+        
+        if(chargeLibre==0)reparateurSelectionne=reparateursDisponibles.get(0);
+    }
+
+}
