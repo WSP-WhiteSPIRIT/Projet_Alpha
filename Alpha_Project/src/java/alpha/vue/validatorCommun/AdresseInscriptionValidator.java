02709d5c11ec0dc9362e055be511c61b07b88a54
INTI0224 <INTI0224@192.168.30.142>
2014-10-03 10:36:55 +0200

    Formulaire inscription

---
+++ Alpha_Project/src/java/alpha/vue/validatorCommun/AdresseInscriptionValidator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alpha.vue.validatorCommun;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author INTI0224
+ */
+public class AdresseInscriptionValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String saisie = (String) value;
+        if (!saisie.isEmpty()) {
+
+        } else {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_WARN, "Adresse invalide", "Ce champ est obligatoire."));
+        }
+    }
+
+}
