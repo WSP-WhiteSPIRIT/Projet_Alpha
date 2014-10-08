8617690ebbe79bb57e60d38b0f285ce1eedee477
INTI0220 <INTI0220@192.168.30.139>
2014-10-08 11:09:17 +0200

    Ajout du formulaire Devis

--- Alpha_Project/src/java/alpha/vue/pagesPubliques/validator/NomInscriptionValidator.java
+++ Alpha_Project/src/java/alpha/vue/pagesPubliques/validator/NomInscriptionValidator.java
@@ -20,11 +20,11 @@ public class NomInscriptionValidator implements Validator {
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         String saisie = (String) value;
-        System.out.println("azeazeaze"+saisie);
+        
         if (!saisie.isEmpty()) {
             
         } else {
-            System.out.println("qer jgnqerkg bsqelgjberg lb qlrejg bqlul");
+            
             throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_WARN, "Nom invalide", "Ce champ est obligatoire."));
         }
     }
